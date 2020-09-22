b0eba059a9a5e4999f034cc1caa780962a232b09
alexrnov <a.novopashin5@gmail.com>
2020-09-22 13:06:35 +0900

    retrofit example

--- app/src/main/java/alexrnov/worldtime/MainActivity.java
+++ app/src/main/java/alexrnov/worldtime/MainActivity.java
@@ -1,14 +1,20 @@
 package alexrnov.worldtime;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +31,26 @@ public class MainActivity extends AppCompatActivity {
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(navView, navController);
+
+
+    ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+    Call<List<Movie>> call = apiService.getMovies();
+
+    call.enqueue(new Callback<List<Movie>>() {
+      @Override
+      public void onResponse(Call<List<Movie>> call, Response<List<Movie>> response) {
+        Log.i("P", "response success = " + response.body());
+        List<Movie> list = response.body();
+        for (Movie movie: list) {
+          Log.i("P", "url = " + movie.getImageUrl() + ", title = " + movie.getTitle());
+        }
+      }
+
+      @Override
+      public void onFailure(Call<List<Movie>> call, Throwable t) {
+        Log.i("P","Response failure= "+t.toString());
+      }
+    });
   }
 
 }
