24f8496533a5a3be5fefe0ae8e0145f27c43a3ec
alexrnov <a.novopashin5@gmail.com>
2020-09-25 10:20:54 +0900

    add location

--- app/src/main/java/alexrnov/worldtime/MainActivity.java
+++ app/src/main/java/alexrnov/worldtime/MainActivity.java
@@ -150,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-      // TODO: Consider calling
+      // Consider calling
       //    ActivityCompat#requestPermissions
       // here to request the missing permissions, and then overriding
       //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
