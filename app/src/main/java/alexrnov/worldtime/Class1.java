d5bd9c8022ae3a0d8193755e73b1c67420f89a8b
alexrnov <a.novopashin5@gmail.com>
2020-10-07 21:27:41 +0300

    commit

--- app/src/main/java/alexrnov/worldtime/Class1.java
+++ app/src/main/java/alexrnov/worldtime/Class1.java
@@ -4,5 +4,8 @@ public class Class1 {
     public static void main(String[] args) {
         // first comment
         // second comment
+        // 3 comment
+        // 4 comment
+        // five comment
     }
 }
